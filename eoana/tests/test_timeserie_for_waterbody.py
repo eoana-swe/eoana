7950a3567cdd9726296021f3de031493c8dfeba6
Johansson Johannes <a002028@ad.smhi.se>
2021-12-07 12:58:19 +0100

    test scripts

--- eoana/tests/test_timeserie_for_waterbody.py
+++ eoana/tests/test_timeserie_for_waterbody.py
@@ -2,7 +2,7 @@
 # Copyright (c) 2020 SMHI, Swedish Meteorological and Hydrological Institute.
 # License: MIT License (see LICENSE.txt or http://opensource.org/licenses/mit).
 """
-Created on 2021-12-07 11:09
+Created on 2021-12-07 11:33
 
 @author: johannes
 """
@@ -17,11 +17,11 @@ from eoana.config import Settings
 
 
 if __name__ == '__main__':
+
     settings = Settings()
 
     sen3_data_l2 = 'C:/Temp/Satellit/sentinel_data'
 
-    # datetime(YEAR, MOMNTH, DAY, HOUR, MINUTE)
     filenames = find_files_and_readers(
         start_time=datetime(2021, 7, 25, 7, 20),
         end_time=datetime(2021, 7, 25, 11, 25),
@@ -33,17 +33,7 @@ if __name__ == '__main__':
     """ Create Scene object """
     scn = Scene(filenames=filenames)
 
-    """ Load selected datasets 
-        Available OLCI Level 2 datasets are:
-            'chl_nn.nc','chl_oc4me.nc',
-            'iop_nn.nc','iwv.nc','par.nc','trsp.nc','tsm_nn.nc','w_aer.nc',
-            'Oa01_reflectance.nc','Oa02_reflectance.nc','Oa03_reflectance.nc','Oa04_reflectance.nc',
-            'Oa05_reflectance.nc','Oa06_reflectance.nc','Oa07_reflectance.nc','Oa08_reflectance.nc',
-            'Oa09_reflectance.nc','Oa10_reflectance.nc','Oa11_reflectance.nc','Oa12_reflectance.nc',
-            'Oa16_reflectance.nc','Oa17_reflectance.nc','Oa18_reflectance.nc','Oa21_reflectance.nc',
-            'wqsf.nc'
-    """
-    datasets = ['Oa08', 'Oa06', 'chl_nn', 'chl_oc4me', 'mask']
+    datasets = ['chl_nn', 'mask']
     scn.load(datasets)
 
     my_area = load_area(
@@ -53,7 +43,5 @@ if __name__ == '__main__':
 
     """ Chlorophyll data are stored as logarithmic values. Convert to real values: """
     scn['chl_nn'] = np.power(10, scn['chl_nn'])
-
     plt.imshow(scn['chl_nn'])
-    plt.colorbar()
-    plt.clim(0, 10)
+
